46a301409271313c559e3c47ce86e5d90318b147
alexander <alexaruba18@gmail.com>
2021-10-10 16:42:23 -0500

    version 1.0

---
+++ app/src/main/java/com/moviles/mercado_pulgas/registerUserActivity.java
@@ -0,0 +1,14 @@
+package com.moviles.mercado_pulgas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
